eb7e9819c1ecbe2b70ad74166277932963f73b10
frsargua <frsargua@gmail.com>
2022-08-15 10:34:01 +0100

    created route and controller for getting single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,12 +27,23 @@ const grade = async (studentId) =>
 module.exports = {
   getThoughts(req, res) {
     Thought.find()
-      .then(async (students) => {
-        return res.json(students);
+      .then(async (thought) => {
+        return res.json(thought);
       })
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
+  },
+
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No course with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
   },
 };
