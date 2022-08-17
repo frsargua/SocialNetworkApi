88fd6ff5f8e7fd46443b881eb2a637bd00e005a3
frsargua <frsargua@gmail.com>
2022-08-17 15:24:15 +0100

    fixed delete Reaction controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,28 +1,5 @@
 // ObjectId() method for converting studentId string into an ObjectId for querying database
-const { ObjectId } = require("mongoose").Types;
 const { Thought, User } = require("../models");
-
-// TODO: Create an aggregate function to get the number of students overall
-const headCount = async () =>
-  Student.aggregate()
-    // Your code here
-    .then((numberOfStudents) => numberOfStudents);
-
-// Execute the aggregate method on the Student model and calculate the overall grade by using the $avg operator
-const grade = async (studentId) =>
-  Student.aggregate([
-    // TODO: Ensure we include only the student who can match the given ObjectId using the $match operator
-    {
-      // Your code here
-    },
-    {
-      $unwind: "$assignments",
-    },
-    // TODO: Group information for the student with the given ObjectId alongside an overall grade calculated using the $avg operator
-    {
-      // Your code here
-    },
-  ]);
 
 module.exports = {
   getThoughts(req, res) {
@@ -118,10 +95,11 @@ module.exports = {
   },
 
   deleteReaction(req, res) {
+    let { reactionId: id } = req.body;
     Thought.findByIdAndUpdate(
       req.params.thoughtId,
       {
-        $pull: { reactions: reactionId },
+        $pull: { reactions: { reactionId: id } },
       },
       { runValidators: true, new: true }
     )
@@ -130,6 +108,15 @@ module.exports = {
           ? res
               .status(404)
               .json({ message: "No student found with that ID :(" })
+          : thought.reactions.some((reaction) => {
+              if (!(reaction.reactionId.toHexString() == id)) {
+                return true;
+              }
+              return false;
+            })
+          ? res
+              .status(404)
+              .json({ message: "No reaction found with this ID :(" })
           : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
